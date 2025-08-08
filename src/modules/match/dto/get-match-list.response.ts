89a317350e2f45500592b3fa1ed3b3853e11295a
HyunWoo Joung <100191598+iHyunWoo@users.noreply.github.com>
2025-08-08 14:31:47 +0900

    Feat: 마킹 테이블 필드 수정

--- src/modules/match/dto/get-match-list.response.ts
+++ src/modules/match/dto/get-match-list.response.ts
@@ -2,6 +2,7 @@ export class GetMatchListResponse {
   id: number;
   tournamentName: string;
   opponentName: string;
+  opponentTeam: string;
   myScore: number;
   opponentScore: number;
   thumbnailUrl: string | null;
