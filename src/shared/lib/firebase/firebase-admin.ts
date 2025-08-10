568b59a78aeba1ff537977399b739c1b1a3d67bf
HyunWoo Joung <100191598+iHyunWoo@users.noreply.github.com>
2025-08-10 21:09:51 +0900

    Chore: gcs key 파일 위치 변경

--- src/shared/lib/firebase/firebase-admin.ts
+++ src/shared/lib/firebase/firebase-admin.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(), // 또는 cert()
+  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS ?? ""),
 });
 
 export { admin };
