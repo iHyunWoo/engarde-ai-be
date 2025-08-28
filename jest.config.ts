a6efe05438f83ee51fb619bdce8a1dda793d3715
HyunWoo Joung <100191598+iHyunWoo@users.noreply.github.com>
2025-08-28 22:30:49 +0900

    Setting: jest config 파일 e2e와 unit 테스트 용 2개로 분리

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,16 @@ const config: Config = {
       tsconfig: 'tsconfig.json',
     },
   },
+  collectCoverage: true, // 커버리지 수집 활성화
+  collectCoverageFrom: [
+    'src/**/*.ts',             // 커버리지 대상 파일들
+    '!api-sdk/',
+    '!src/main.ts',            // 제외할 파일
+    '!src/**/dto/**',          // DTO 제외 등
+    '!src/test/',           // 테스트 파일 제외
+    '!src/types/'
+  ],
+  coverageDirectory: 'coverage', // 커버리지 결과 저장 폴더
 };
 
 export default config;
