7ad87bbb145350d8949f20623634c8c8ded30c69
HyunWoo Joung <100191598+iHyunWoo@users.noreply.github.com>
2025-08-28 16:42:55 +0900

    Refactor: prisma schema에서 map 속성들 제거 후 migrate

--- prisma/migrations/20250818051959_remove_technique_type_optional_soft_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `type` on table `Technique` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Technique" ALTER COLUMN "type" SET NOT NULL;
